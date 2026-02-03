06d79726bae50e30733b9a0bf229506781719b01
Max Cerny <info@maxinthecloud.com>
2026-02-04 00:49:24 +0100

    Use Azure AD auth for backend storage

--- terraform/tst/backend.tf
+++ terraform/tst/backend.tf
@@ -4,6 +4,7 @@ terraform {
         storage_account_name = "sttfstatemax1tst"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
-        use_oidc             = true
+        use_oidc            = true
+        use_azuread_auth    = true
     }
 }
